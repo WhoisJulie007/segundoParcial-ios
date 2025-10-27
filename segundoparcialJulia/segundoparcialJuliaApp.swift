b7bf3341df2d60ad8d1915540108f5b06c08c856
Julia <maydeli.castan@iest.edu.mx>
2025-10-27 16:40:21 -0600

    Initial Commit

---
+++ segundoparcialJulia/segundoparcialJuliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  segundoparcialJuliaApp.swift
+//  segundoparcialJulia
+//
+//  Created by WIN603 on 27/10/25.
+//
+
+import SwiftUI
+
+@main
+struct segundoparcialJuliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
